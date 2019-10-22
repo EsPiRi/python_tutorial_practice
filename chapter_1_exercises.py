cf9f030afef63529228d06d65b72a4fac485662b
EsPiRi <sezginsami@gmail.com>
2019-10-23 00:57:02 +0300

    Heinold A Practical Introduction to Python Programming Exercises Solution

--- chapter_1_exercises.py
+++ chapter_1_exercises.py
@@ -27,3 +27,19 @@ print("The square of %d is %d" % (number, number ** 2))
 print()
 number = int(input("Enter a number: "))
 print(number, 2 * number, 3 * number, 4 * number, 5 * number, sep="---")
+
+print()
+kg = int(input("Enter your weight: "))
+print("Your weight is %d kg converted to %d pounds" % (kg, kg * 2.2))
+
+print()
+num1, num2, num3 = int(input("Enter 1/3 number: ")), int(input("Enter 2/3 number: ")), int(input("Enter 3/3 number: "))
+total = num1+num2+num3
+average = total / 3
+print("total: %d \naverage: %d" % (total, average))
+
+print()
+price=int(input("Enter the meal of the price: "))
+tip=int(input("Enter the tip percent: "))
+total=price+price*tip/100
+print("Tip: %d\nTotal bill: %d" %(tip,total))
