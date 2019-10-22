6cad1f6ea93d270e836dcade5da14eec3c462270
EsPiRi <sezginsami@gmail.com>
2019-10-23 00:46:13 +0300

    Heinold A Practical Introduction to Python Programming Exercises Solution

--- chapter_1_exercises.py
+++ chapter_1_exercises.py
@@ -1,4 +1,29 @@
 for i in range(4):
-    for i in range(19):
-        print("*",end="")
+    for j in range(19):
+        print("*", end="")
     print()
+
+print()
+for i in range(4):
+    if i % 3 == 0:
+        print("*" * 19)
+    else:
+        print("*", end="");
+        print(" " * 17, end="");
+        print("*", end="\n")
+
+print()
+for i in range(4):
+    print("*" * (i + 1))
+
+print()
+result = (512 - 282) / (47 * 48 + 5)
+print(result)
+
+print()
+number = int(input("Enter a number: "))
+print("The square of %d is %d" % (number, number ** 2))
+
+print()
+number = int(input("Enter a number: "))
+print(number, 2 * number, 3 * number, 4 * number, 5 * number, sep="---")
