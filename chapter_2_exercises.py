16523342d6ae3aa2c5a2982089a7df0f5434c78a
EsPiRi <sezginsami@gmail.com>
2019-10-23 15:52:35 +0300

    Heinold A Practical Introduction to Python Programming Exercises Solution

--- chapter_2_exercises.py
+++ chapter_2_exercises.py
@@ -28,3 +28,55 @@ for i in range(100, 0, -2):
 
 # 7
 print()
+for i in range(10):
+    print("A", end="")
+for i in range(7):
+    print("B", end="")
+for i in range(4):
+    print("C", end="")
+    print("D", end="")
+print("E", end="")
+for i in range(5):
+    print("F", end="")
+print("G", end="")
+
+# 8
+print()
+name = input("Enter your name: ")
+number = int(input("Enter how many times you want to print your name: "))
+for i in range(number):
+    print(name)
+
+# 9
+print()
+number = int(input("Enter how many numbers you want to print Fibonacci series: "))
+a = 1
+b = 1
+for i in range(number):
+    print(a, end=",")
+    a, b = b, a + b
+
+# 10
+print()
+width = int(input("Enter the width of the box: "))
+height = int(input("Enter the height of the box: "))
+for i in range(height):
+    print("*" * width)
+
+# 11
+print()
+width = int(input("Enter the width of the box: "))
+height = int(input("Enter the height of the box: "))
+for i in range(height):
+    if i % (height - 1) == 0:
+        print("*" * width)
+    else:
+        print("*", end="");
+        print(" " * (width - 2), end="");
+        print("*", end="\n")
+
+# 12
+print()
+height = int(input("Enter the triangle height: "))
+for i in range(1, height + 1):
+    print("*" * i)
