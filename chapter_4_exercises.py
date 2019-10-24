f3814b4c4a4bca169f78ff169262dd9a3b0e8511
EsPiRi <sezginsami@gmail.com>
2019-10-24 23:56:50 +0300

    Heinold A Practical Introduction to Python Programming Exercises Solution

--- chapter_4_exercises.py
+++ chapter_4_exercises.py
@@ -5,7 +5,16 @@ if cm<0:
 else:
     print("Centimeters: %d, Inches: %.2f" %(cm,cm/2.54))
 
-# 2
+#2
+print()
+temperature=float(input("Enter a temperature: "))
+selection=int(input("Which unit you want to use?\n1. Celsius\n2. Fahrenheit\n"))
+if selection==1:
+    print("The temperature in Fahrenheit is",(9/5)*temperature+32)
+if selection==2:
+    print("The temperature in Celsius is", (5/9)*(temperature-32))
+
+# 3
 print()
 temperature = float(input("Enter a temperature in Celsius: "))
 if temperature < -273.15:
@@ -22,3 +31,4 @@ elif temperature == 100:
     print("The temperature is at the boiling point")
 elif temperature > 100:
     print("The temperature is above the boiling point")
+
--- temp.py
+++ temp.py
@@ -3,3 +3,11 @@ from random import random
 import random
 from math import *
 
+#2
+print()
+temperature=float(input("Enter a temperature: "))
+selection=int(input("Which unit you want to use?\n1. Celsius\n2. Fahrenheit\n"))
+if selection==1:
+    print("The temperature in Fahrenheit is",(9/5)*temperature+32)
+if selection==2:
+    print("The temperature in Celsius is", (5/9)*(temperature-32))
