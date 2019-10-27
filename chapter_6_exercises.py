75a219d951d3ca864d7ad52e012616fe827e161a
EsPiRi <sezginsami@gmail.com>
2019-10-28 00:08:07 +0300

    Heinold A Practical Introduction to Python Programming Exercises Solution

--- chapter_6_exercises.py
+++ chapter_6_exercises.py
@@ -18,12 +18,40 @@ if len(input_) >= 7:
 else:
     print("The string is not long enough to print that character")
 
-#h
-removed_string=input_[1:len(input_)-1]
+# h
+removed_string = input_[1:len(input_) - 1]
 print(removed_string)
-#i
+# i
 print(input_.upper())
-#j
-print(input_.replace("a","e"))
-#k
-print(input_.replace(input_," "))
+# j
+print(input_.replace("a", "e"))
+# k
+print(input_.replace(input_, " "))
+
+# 2
+print()
+sentence = input("Enter a text: ")
+print(sentence.count(" ") + 1)
+
+# 3
+print()
+formula = input("Enter a formula: ")
+opening_parenthesis = formula.count("(")
+closing_parenthesis = formula.count(")")
+if opening_parenthesis == closing_parenthesis:
+    print("Same number of parenthesis")
+else:
+    print("Different number of parenthesis", opening_parenthesis, closing_parenthesis)
+
+# 4
+print()
+word = input("Enter a word: ")
+contains_vowel = False
+for letter in ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]:
+    if letter in word:
+        contains_vowel = True
+
+if contains_vowel:
+    print("This word contains at least one vowel.")
+else:
+    print("This word doesn't contain any vowel")
--- temp.py
+++ temp.py
@@ -4,5 +4,14 @@ from random import random
 import random
 from math import *
 
-#2
+#4
+word=input("Enter a word: ")
+contains_vowel=False
+for letter in ["a","e","i","o","u","A","E","I","O","U"]:
+    if letter in word:
+        contains_vowel=True
 
+if contains_vowel:
+    print("This word contains at least one vowel.")
+else:
+    print("This word doesn't contain any vowel")
